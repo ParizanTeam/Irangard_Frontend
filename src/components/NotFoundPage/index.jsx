fc18368a800a8d2e80b90f8487caffae96cdfa98
Bayan <bayany1379@gmail.com>
2022-03-06 00:23:22 +0330

    [chore]: Init project and do basic configs

---
+++ src/components/NotFoundPage/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NotFoundPage = () => (
+  <div>
+    <div style={{ display: "flex", justifyContent: "center", margin: "100px" }}>
+      اینجا کجاس من کجام
+    </div>
+  </div>
+);
+
+export default NotFoundPage;
