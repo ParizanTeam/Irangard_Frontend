951636f900848bb22205b6b6548d4d06ecccc49e
Emadmousavi <emadmoosavi79@gmail.com>
2022-04-08 17:17:39 +0430

    [chore] add new homepage-banner

--- src/components/SearchBar/index.jsx
+++ src/components/SearchBar/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import searchBanner from 'src/assets/images/searchBanner.webp';
+import image from 'src/assets/images/main1.jpg';
 import './style.scss';
 
 const SearchBar = () => {
   return (
     <div className="search-bar">
-      <img src={searchBanner} alt="nature" className="search-bar__img" />
+      {/* <img src={searchBanner} alt="nature" className="search-bar__img" /> */}
+      <img src={image} alt="nature" className="main-banner" />
       <input className="search-bar__search-box" placeholder="جست‌وجو برای مقصد..." />
     </div>
   );
