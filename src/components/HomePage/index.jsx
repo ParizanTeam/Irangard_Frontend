42ca4491c90111aa239771b4bf05e162a992a259
Afshin Zanganeh <afshinzanganeh2001@gmail.com>
2022-04-17 12:27:19 +0430

    [feat]: add map component

--- src/components/HomePage/index.jsx
+++ src/components/HomePage/index.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import Helmet from 'react-helmet';
 import Header from 'src/components/Header';
 import CitySlider from 'src/components/CitySlider';
-import Map from 'src/components/Map';
+import IranMap from 'src/components/IranMap';
 import SearchBar from 'src/components/SearchBar/index';
 import ExperienceSlider from 'src/components/ExperienceSlider/index';
 import Footer from 'src/components/Footer';
-import Testimonial from '../Testimonial';
+import Testimonial from 'src/components/Testimonial';
 
 const HomePage = () => {
   return (
@@ -17,7 +17,7 @@ const HomePage = () => {
       <Header />
       <SearchBar />
       <CitySlider />
-      <Map />
+      <IranMap />
       <Testimonial />
       <ExperienceSlider />
       <Footer />
