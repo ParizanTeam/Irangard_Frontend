1d07aeab578b8fd5d712615758705f5e12890c72
Afshin Zanganeh <afshinzanganeh2001@gmail.com>
2022-04-22 13:30:18 +0430

    [feat]: connect profile to back

--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -1,28 +1,73 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { RiSettings5Line } from 'react-icons/ri';
+import toast, { Toaster } from 'react-hot-toast';
+import { Modal } from '@mui/material';
 import { convertNumberToPersian } from '../../utils/formatters';
 import Layout from '../Layout';
-import profileImg from '../../assets/images/profile.jpeg';
+import { useGetProfile, usePutProfile } from '../../api/profile';
 import './style.scss';
-import { Modal } from '@mui/material';
 
 const Profile = () => {
-  const { username } = useParams();
+  const { username: usernameQuery } = useParams();
+  const [formData, setFormData] = useState({});
+  const [errors, setErrors] = useState({});
+  const [updateLoading, setUpdateLoading] = useState(false);
+  const { isLoading, error, data } = useGetProfile(usernameQuery);
+  const imageRef = useRef(null);
+  useEffect(() => {
+    if (data) {
+      setFormData(data);
+    }
+  }, [data]);
   const [editProfileModalOpen, setEditProfileModalOpen] = useState(false);
-  const { followers = 0, followings = 0, profile = profileImg } = {};
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  const { followers, followings, full_name, email, username, about_me, profileImg } = formData;
+
   const handleOpen = () => {
     setEditProfileModalOpen(true);
   };
   const handleClose = () => {
     setEditProfileModalOpen(false);
   };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    const body = new FormData();
+    for (const key in formData) {
+      body.append(key, formData[key]);
+    }
+    setUpdateLoading(true);
+    usePutProfile(
+      usernameQuery,
+      body,
+      error => {
+        if (error.response.status === 400) {
+          setErrors({ username: 'نام کاربری توسط شخص دیگری انتخاب شده‌است.' });
+        } else {
+          toast.error('مشکلی در سامانه رخ داده‌است.');
+        }
+        setUpdateLoading(false);
+      },
+      data => {
+        console.log(data);
+        setErrors({});
+        setUpdateLoading(false);
+        toast.success('اطلاعات با موفقیت تغییر یافت');
+        setEditProfileModalOpen(false);
+      }
+    );
+  };
+
   return (
     <Layout title={`پروفایل ${username}`}>
+      <Toaster />
       <div className="profile">
         <div className="profile-summary">
           <div className="profile-summary__img">
-            <img src={profile} alt={username} />
+            <img src={profileImg} alt={username} />
           </div>
           <div className="profile-summary__follow">
             <div className="profile-summary__followers">
@@ -49,32 +94,78 @@ const Profile = () => {
         >
           <form className="edit-profile">
             <h2>ویرایش پروفایل</h2>
-            <div className="edit-profile__img">
-              <img src={profile} alt="" />
+            <div className="edit-profile__img" onClick={() => imageRef.current.click()}>
+              <img src={profileImg} />
+              <input
+                type="file"
+                ref={imageRef}
+                style={{ display: 'none' }}
+                onChange={e => {
+                  setFormData(old => ({
+                    ...old,
+                    image: e.target.files[0],
+                    profileImg: URL.createObjectURL(e.target.files[0]),
+                  }));
+                }}
+                accept="image/*"
+              />
               <div className="edit-profile__img-edit">ویرایش</div>
             </div>
             <div className="edit-profile__form-control">
               <label htmlFor="name">نام و نام خانوادگی:</label>
               <br />
-              <input type="text" id="name" name="name" placeholder="نام و نام خانوادگی" />
+              <input
+                type="text"
+                id="name"
+                name="name"
+                placeholder="نام و نام خانوادگی"
+                value={full_name}
+                onChange={e => setFormData(old => ({ ...old, full_name: e.target.value }))}
+              />
             </div>
             <div className="edit-profile__form-control">
               <label htmlFor="username">نام کاربری:</label>
               <br />
-              <input type="text" id="username" name="username" placeholder="نام کاربری" />
+              <input
+                type="text"
+                id="username"
+                name="username"
+                placeholder="نام کاربری"
+                value={username}
+                onChange={e => setFormData(old => ({ ...old, username: e.target.value }))}
+              />
+              {errors.username && <p className="edit-profile__error-msg">{errors.username}</p>}
             </div>
             <div className="edit-profile__form-control">
               <label htmlFor="email">ایمیل:</label>
               <br />
-              <input type="text" id="email" name="email" placeholder="ایمیل" />
+              <input
+                type="text"
+                id="email"
+                name="email"
+                placeholder="ایمیل"
+                value={email}
+                onChange={e => setFormData(old => ({ ...old, email: e.target.value }))}
+                readOnly
+              />
             </div>
             <div className="edit-profile__form-control">
               <label htmlFor="about">درباره من</label>
               <br />
-              <textarea name="about" id="about" cols="30" rows="6" placeholder="درباره من"></textarea>
+              <textarea
+                name="about"
+                id="about"
+                cols="30"
+                rows="6"
+                placeholder="درباره من"
+                value={about_me}
+                onChange={e => setFormData(old => ({ ...old, about_me: e.target.value }))}
+              ></textarea>
             </div>
-            <button className="edit-profile__save-btn">ثبت تغییرات</button>
-            <button className="edit-profile__cancel-btn" onClick={handleClose}>
+            <button className="edit-profile__save-btn" onClick={handleSubmit} disabled={updateLoading}>
+              ثبت تغییرات
+            </button>
+            <button className="edit-profile__cancel-btn" onClick={handleClose} disabled={updateLoading}>
               انصراف
             </button>
           </form>
