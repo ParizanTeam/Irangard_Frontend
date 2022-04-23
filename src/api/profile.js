3f02d71ac40a80da16d36344603f84c0ab938cbb
Afshin Zanganeh <afshinzanganeh2001@gmail.com>
2022-04-23 17:08:45 +0430

    [feat]: add authorization to edit profile

--- src/api/profile.js
+++ src/api/profile.js
@@ -1,15 +1,28 @@
 import axios from 'axios';
-import { useQuery, useMutation } from 'react-query';
+import { useQuery } from 'react-query';
 import { baseUrl } from '../utils/constants';
 import defaultProfileImg from '../assets/images/profile.jpeg';
 
 export const useGetProfile = username =>
-  useQuery('getProfile', () =>
-    axios
-      .get(`${baseUrl}/accounts/profile/${username}`)
+  useQuery('getProfile', () => {
+    const access_token = localStorage.getItem('access_token') || '';
+    return axios
+      .get(`${baseUrl}/accounts/profile/${username}`, {
+        headers: {
+          Authorization: 'JWT ' + access_token,
+        },
+      })
       .then(res => res.data)
       .then(data => {
-        const { followers = 0, followings = 0, full_name = '', email = '', username = '', about_me = '' } = data;
+        const {
+          followers = 0,
+          followings = 0,
+          full_name = '',
+          email = '',
+          username = '',
+          about_me = '',
+          is_owner = false,
+        } = data;
         const profileImg = data?.image ? `${baseUrl}${data.image}` : defaultProfileImg;
         return {
           followers,
@@ -19,13 +32,19 @@ export const useGetProfile = username =>
           username,
           about_me,
           profileImg,
+          is_owner,
         };
-      })
-  );
+      });
+  });
 
 export const usePutProfile = async (username, body, onError, onSuccess) => {
   try {
-    const res = await axios.put(`${baseUrl}/accounts/profile/${username}/`, body);
+    const access_token = localStorage.getItem('access_token') || '';
+    const res = await axios.put(`${baseUrl}/accounts/profile/${username}/`, body, {
+      headers: {
+        Authorization: 'JWT ' + access_token,
+      },
+    });
     const data = await res.data;
     onSuccess(data);
   } catch (error) {
