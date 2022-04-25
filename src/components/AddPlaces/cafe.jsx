380d54485c7dc03e03f3660f4b463dcfaf246dd0
Hanieh Jafari <hani.j.2000@gmail.com>
2022-04-25 17:10:05 +0430

    [fix]: change backgrounds

--- src/components/AddPlaces/cafe.jsx
+++ src/components/AddPlaces/cafe.jsx
@@ -33,7 +33,7 @@ const { mutateAsync, isLoading } = useMutation(loginData =>
   };
 
   return (
-    <div className='mainC'>
+    <div className='main'>
       <div className='sub'>
         <div className='header'>
           <img src={header} alt="header" className='HeeadImg'/>
