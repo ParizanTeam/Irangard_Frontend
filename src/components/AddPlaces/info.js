e8901ab3b853821ba60008036e5eb98c3861a635
Bayan <bayany1379@gmail.com>
2022-04-25 20:13:57 +0430

    [fix]: add-place cafe

--- src/components/AddPlaces/info.js
+++ src/components/AddPlaces/info.js
@@ -1,13 +1,23 @@
+
+// cafe 0
+// hotel 1
+// tafrihi 2
+// didani 3
+
+
 export const IranCities = {
   Tehran: [
     { label: 'تهران', value: 'Tehran' },
     { label: 'رباط کریم', value: 'RobatKarim' },
   ],
   Shiraz: [
-    { label: 'شیراز', value: 'Shiraz' },
+    { label: 'شیراز', value: 'Tehran' },
   ],
 };
 export const IranStates = [
   { label: 'استان تهران', value: 'Tehran' },
   { label: 'استان شیراز', value: 'Shiraz' },
 ];
+
+
+export const CafeTags = ["قلیان"];
