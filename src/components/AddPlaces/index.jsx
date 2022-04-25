ca0fd5fdfa649818d34692730f3a626f235e4e44
Bayan <bayany1379@gmail.com>
2022-04-25 11:32:55 +0430

    [fix]: add-place starter form

--- src/components/AddPlaces/index.jsx
+++ src/components/AddPlaces/index.jsx
@@ -8,25 +8,12 @@ import Header from 'src/components/Header';
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
 import { IranStates, IranCities } from './info.js';
+import { Chip } from '@mui/material';
+import { RiRestaurantLine, RiHome3Line, RiChatQuoteLine, RiShipLine } from 'react-icons/ri';
 
-const styles = theme => ({
-  textField: {
-    width: '90%',
-    marginLeft: 'auto',
-    marginRight: 'auto',
-    paddingBottom: 0,
-    marginTop: 0,
-    fontWeight: 500,
-  },
-  input: {
-    color: 'white',
-  },
-});
-
-export default function Places() {
+const CafeForm = () => {
   return (
-    <div className="main">
-      <Header />
+    <div>
       <div className="header">
         <img src={headerImg} alt="header" className="HeeadImg" />
       </div>
@@ -44,18 +31,24 @@ export default function Places() {
             sx={{ width: 300 }}
             renderInput={params => (
               <div ref={params.InputProps.ref} className="basic-field">
-                <input  {...params.inputProps} className="field-input" type="text" id="state" placeholder="استان را انتخاب کنید." />
+                <input
+                  {...params.inputProps}
+                  className="field-input"
+                  type="text"
+                  id="state"
+                  placeholder="استان را انتخاب کنید."
+                />
               </div>
             )}
           />
         </div>
 
-        {/* <div className="form__group1 field">
+        <div className="form__group1 field">
           <input type="input" className="form__field1" id="about" placeholder="درباره" />
           <label htmlFor="about" className="form__label1">
             درباره
           </label>
-        </div> */}
+        </div>
 
         <div className="basic-field">
           <label htmlFor="street-address" className="field__label">
@@ -92,9 +85,72 @@ export default function Places() {
         </div>
         <input className="submit-btn2" type="submit" value="ثبت مکان" />
       </form>
-      <div className="map">
-        <img src={AddPlacesImg} alt="AddPlaces" style={{ width: '180px', marginRight: '20px' }} />
-      </div>
+    </div>
+  );
+};
+export default function Places() {
+  const [place, setPlace] = React.useState(null);
+  const Forms = {
+    0: <CafeForm />,
+  };
+
+  return (
+    <div className="main add-place">
+      <Header />
+      {!place ? (
+        <div className="starter-section">       
+          <div className="title">
+            <h1>اضافه‌کردن مکان جدید به ایرانگرد</h1>
+          </div>
+
+          <div className="description">
+            <h5>
+              مرسی که تصمیم گرفتی بهمون کمک کنی مکان جدید به ‌ایرانگرد اضافه بشه. برای شروع بهون یکمی درمورد مکان بگو
+            </h5>
+          </div>
+
+          <div className="question" dir="ltr">
+            <h3>مکان ات چه جور جایی است؟</h3>
+            <div className="options">
+              <span>
+                <Chip
+                  onClick={() => setPlace(1)}
+                  icon={<RiRestaurantLine size={24} />}
+                  label="رستوران‌‌ یا کافه"
+                  variant="outlined"
+                />
+              </span>
+              <span>
+                <Chip
+                  onClick={() => setPlace(1)}
+                  icon={<RiHome3Line size={24} />}
+                  label="اقامتگاه"
+                  variant="outlined"
+                />
+              </span>
+              <span>
+                <Chip
+                  onClick={() => setPlace(1)}
+                  icon={<RiShipLine size={24} />}
+                  label="مرکز تفریحی"
+                  variant="outlined"
+                />
+              </span>
+              <span>
+                <Chip
+                  onClick={() => setPlace(1)}
+                  icon={<RiChatQuoteLine size={24} />}
+                  label="جاذبه‌دیدنی"
+                  variant="outlined"
+                />
+              </span>
+            </div>
+          </div>
+
+        </div>
+      ) : (
+        <CafeForm />
+      )}
     </div>
   );
 }
