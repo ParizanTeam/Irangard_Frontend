ae589e7dffde122a1d308a397e49c6d22a004f5a
Afshin Zanganeh <afshinzanganeh2001@gmail.com>
2022-05-04 12:26:10 +0430

    [feat]: add button component

---
+++ src/components/Button/index.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import classNames from 'classnames';
+import './style.scss';
+
+function Button({ type = 'button', variant = 'black', className, children, ...props }) {
+  return (
+    <button
+      className={classNames(
+        'button',
+        {
+          orange: variant === 'orange',
+          black: variant === 'black',
+          blue: variant === 'blue',
+          green: variant === 'green',
+          red: variant === 'red',
+          purple: variant === 'purple',
+        },
+        className
+      )}
+      type={type}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+}
+
+export default Button;
