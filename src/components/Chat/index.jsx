c44cc89780a1a7c860bc382736226e02386cf53d
Emadmousavi <emadmoosavi79@gmail.com>
2022-07-08 01:05:55 +0430

    [chore] minor changes

---
+++ src/components/Chat/index.jsx
@@ -0,0 +1,18 @@
+import React, { useEffect, useRef, useState } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import toast, { Toaster } from 'react-hot-toast';
+import { Modal } from '@mui/material';
+import './style.scss';
+
+function Chat(){
+
+
+
+    return(
+        <div>
+            <h1>hello chat</h1>
+        </div>
+    )
+}
+
+export default Chat;
