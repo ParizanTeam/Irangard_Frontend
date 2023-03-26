8ba24416e7ca2b810ef2ccad22234ce2ac098e3a
Seyed Mahdi Hosseini Zade <mahdihosseinizade2000@gmail.com>
2023-03-26 15:02:06 +0430

    change domain for solve corsPolicy

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,3 @@
 export const baseUrl = 'http://188.121.102.52';
-// export const baseUrl = 'https://api.parizaan.ir';
+// export const baseUrl = 'https://api.quilco.ir';
 // export const baseUrl="https://irangard-backend.herokuapp.com"
